42ba05fa33a6debf5636a4966e74261f7583a2d4
Mika28110 <michael.godinhodacosta@hotmail.fr>
2015-09-16 11:55:05 +0200

    création du header séparé

---
+++ includes/header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="css/owl.carousel.css" />
+	<link rel="stylesheet" href="css/flexslider.css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,700,600,300' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2">
+					<h2><span>KM120</span><br />.CYCLES</h2>
+				</div>
+
+				<div class="col-md-6 col-md-offset-4 header-right">
+				<h3>Gérald MILTGEN</br />Réparation et ventes de vélos à La Loupe</h3>
+				</div>
+			</div>
+		</div>
+
+			<nav class="navbar navbar-default affix-top" role="navigation" id="topnavbar">
+			  <div class="container-fluid">
+			    <!-- Brand and toggle get grouped for better mobile display -->
+			    <div class="navbar-header">
+			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			      </button>
+		      		<a class="navbar-brand" href="#">Brand</a>
+			    </div>
+
+			    <!-- Collect the nav links, forms, and other content for toggling -->
+			    <div class="collapse navbar-collapse pull-right" id="bs-example-navbar-collapse-1">
+			      <ul class="nav navbar-nav">
+			        <li><a href="#">Accueil</a></li>
+			        <li><a href="#">Atelier</a></li>
+			        <li><a href="#">Ventes</a></li>
+			        <li><a href="#">Qui suis-je</a></li>
+			        <li><a href="#">Contact</a></li>
+			       </ul>
+			    </div><!-- /.navbar-collapse -->
+			  </div><!-- /.container-fluid -->
+			</nav>
+	</header>
+
+	<div id="carousel-example-generic" class="carousel slide vertical" data-ride="carousel">
+			  <!-- Wrapper for slides --> 
+		<div class="carousel-inner" role="listbox">
+		    <div class="item active photos">
+		      <img src="images/1.jpg" alt="...">
+		    </div>
+		    <div class="item photos">
+		      <img src="images/2.jpg" alt="...">
+		    </div>
+		   	<div class="item photos">
+		      <img src="images/3.jpg" alt="...">
+		    </div>
+		 </div>
