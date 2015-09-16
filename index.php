81a273d2a2ada43d233265d03420a86f49ecf935
Mika28110 <michael.godinhodacosta@hotmail.fr>
2015-09-16 09:49:14 +0200

    modification slider

--- index.php
+++ index.php
@@ -51,29 +51,30 @@
 			</nav>
 	</header>
 
-	<div id="container">
-		<!-- slider -->
-		<div id="slider">
-			<div class="flexslider">
-				<ul class="slides">
-					<li>
-						<img alt="" src="upload/flexslider/1.jpg" />
-					</li>
-					<li>
-						<img alt="" src="upload/flexslider/2.jpg" />
-					</li>
-					<li>
-						<img alt="" src="upload/flexslider/3.jpg" />
-					</li>
-					<li>
-						<img alt="" src="upload/flexslider/4.jpg" />
-					</li>
-				</ul>
-		    </div>
-		</div>
-		<!-- End slider -->	
-	</div>
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+	  <!-- Indicators -->
+	  <ol class="carousel-indicators">
+	    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+	  </ol>
 
+	  <!-- Wrapper for slides -->
+	  <div class="carousel-inner" role="listbox">
+	    <div class="item active">
+	      <img src="..." alt="...">
+	      <div class="carousel-caption">
+	        ...
+	      </div>
+	    </div>
+	    <div class="item">
+	      <img src="..." alt="...">
+	      <div class="carousel-caption">
+	        ...
+	      </div>
+	    </div>
+	    ...
+	  </div>
 
 	
 	<?php
