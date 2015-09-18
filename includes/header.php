b2fe3d3c3ffbbb75b4d5c0337f97fcab8da3c0fc
Ishimura413 <superouiouil@gmail.com>
2015-09-18 11:14:23 +0200

    ajout de la page atelier et son css

--- includes/header.php
+++ includes/header.php
@@ -11,6 +11,7 @@
 	<link rel="stylesheet" href="css/style.css" />
 	<link rel="stylesheet" href="css/owl.carousel.css" />
 	<link rel="stylesheet" href="css/flexslider.css" />
+	<link rel="stylesheet" href="css/atelier.css" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,700,600,300' rel='stylesheet' type='text/css'>
 </head>
