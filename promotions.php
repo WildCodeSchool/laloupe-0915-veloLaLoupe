37b95e1549e5c9ba9c4bb04c5330ba4851e84ace
Ishimura413 <superouiouil@gmail.com>
2015-09-21 18:22:26 +0200

    page promo et image ok, header et style.css  modifiés

---
+++ promotions.php
@@ -0,0 +1,156 @@
+<?php
+	
+	$menu_actif = 4;
+
+    include('includes/header.php');
+
+    ?>
+
+<body>
+	<div class="container-fluid">
+			
+		<div class="container">
+				<div class="row bienvenue">
+			        <div class="col-md-12">
+			          <h2 class="text-center">Les promotions<span>KM120.CYCLE</span></h2>
+
+			          <p class="sous-titre">Nos promotions du moment !</p> 
+
+			        </div>
+		      	</div>
+		      							<!-- ----- 1ere ligne ----- -->
+			<div id="bloc_promo">
+		      	<div class="row">
+
+		      		<div class="cadre col-lg-3" >
+		      			<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+		      			<p class="descriptif_photo">Descriptif de la promotion</p>
+		      		</div>
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+ 
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+		      	</div>
+
+		      							<!-- ----- 2eme ligne ----- -->
+		      	<div class="row">
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+
+		      	</div>
+
+
+		      							<!-- ----- 3eme ligne ----- -->
+		      	<div class="row">
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+
+		      	</div>
+
+
+		      							<!-- ----- 4eme ligne ----- -->
+		      	<div class="row">
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>		      			
+		      		</div>
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+
+		      	</div>
+
+
+		      							<!-- ----- 5eme ligne ----- -->
+		      	<div class="row">
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>      			
+		      		</div>
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+
+		      		<div class="cadre col-lg-3">
+			      		<img src="images/promo.jpeg" alt="photo_promotion" class="photo_promo">
+			      		<p class="descriptif_photo">Descriptif de la promotion</p>	      			
+		      		</div>
+
+		      	</div>
+		    </div>
+		</div>
+	</div>
+
+<?php
+
+    include('includes/footer.php');
+
+?>
